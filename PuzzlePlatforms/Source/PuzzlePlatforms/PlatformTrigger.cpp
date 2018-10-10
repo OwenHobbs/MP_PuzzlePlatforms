3e71a53ff7c70ddb55ff70382a2fb878f08aa316
Owen Hobbs <owenhobbs2006@gmail.com>
2018-10-09 20:47:12 -0500

    Handling Overlap Events in C++

--- PuzzlePlatforms/Source/PuzzlePlatforms/PlatformTrigger.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PlatformTrigger.cpp
@@ -11,7 +11,7 @@ APlatformTrigger::APlatformTrigger()
 	PrimaryActorTick.bCanEverTick = true;
 
 	TriggerVolume = CreateDefaultSubobject<UBoxComponent>(FName("TriggerVolume"));
-	if (!ensure(TriggerVolume != nullptr)) return;\
+	if (!ensure(TriggerVolume != nullptr)) return;
 
 	RootComponent = TriggerVolume;
 }
@@ -20,10 +20,25 @@ APlatformTrigger::APlatformTrigger()
 void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (!ensure(TriggerVolume != nullptr)) return;
+
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called every frame
 void APlatformTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) 
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated"));
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated"));
 }
