ebca84127ea832298b5ea3686ae593464e69aed9
Owen Hobbs <owenhobbs2006@gmail.com>
2018-10-10 21:31:38 -0500

    Joining Servers With ClientTravel

--- PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -19,7 +19,7 @@ void UPuzzlePlatformsGameInstance::Host()
 	UEngine* Engine = GetEngine();
 	if (!ensure(Engine != nullptr)) return;
 
-	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+	Engine->AddOnScreenDebugMessage(-1, 2, FColor::Green, TEXT("Hosting"));
 
 	UWorld* World = GetWorld();
 	if (!ensure(World != nullptr)) return;
@@ -32,5 +32,10 @@ void UPuzzlePlatformsGameInstance::Join(const FString& Address)
 	UEngine* Engine = GetEngine();
 	if (!ensure(Engine != nullptr)) return;
 
-	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+	Engine->AddOnScreenDebugMessage(-1, 2, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(*Address, ETravelType::TRAVEL_Absolute);
 }
